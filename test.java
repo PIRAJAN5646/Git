c13585968cfa7d39f2c6b707f84b0d3c9acae960
prajan <pirajans11@gmail.com>
2026-03-05 08:11:49 +0530

    changes

--- test.java
+++ test.java
@@ -2,6 +2,5 @@ class test{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a test.");
-        System.out.println("This is another test.");
     }
 }
